5f7bc243e0e68e2ac51fca52bbc0ff5495faf1f1
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-05-29 10:28:59 +0530

    [Rupesh].Added file

---
+++ Ab.java
@@ -0,0 +1,31 @@
+class Ab
+{
+	public static void main(String args[])
+	{
+		int last=0,n=5;
+		for(int i=0;i<=n;i++)
+		{
+			last=last+i;
+			int l=i;
+			for(int j=last;l>0;l--)
+			{
+				System.out.print(j--);
+				if(l>1)
+				{
+					System.out.print("*");
+				}			
+			}
+			System.out.println();
+		}
+	}
+	
+}
+
+/*
+1
+3*2
+4*5*6
+10*9*8*7
+11*12*13*14*15
+
+*/
